473a5e23f3e03fb510f1c3b699ce061978bad0a5
ATanshumantiwari <anshumanat@gmail.com>
2025-07-30 17:34:58 +0530

    test: verified getMenu RPC via Postman

--- backend/src/controllers/getMenu.js
+++ backend/src/controllers/getMenu.js
@@ -1,5 +1,8 @@
 const knex = require('../db');
 
+
+// getMenu tested via Postman on localhost
+
 module.exports = async function getMenu(){
     const items = await knex('menus').select('*');
     return items;
