770f0eee5602b54a4cbe550a5a770c0b8afaf146
ATanshumantiwari <anshumanat@gmail.com>
2025-08-11 01:30:46 +0530

    FIX: Configure production database connection in knexfile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,25 +1,38 @@
-// Update with your config settings.
-
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
 
+  // This block is for local machine
   development: {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'admin1', 
-    database: 'food_ordering',
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: 'admin1',
+      database: 'food_ordering',
+    },
+    migrations: {
+      directory: './src/db/migrations'
+    },
+    seeds: {
+      directory: './src/db/seed'
+    }
   },
-  migrations: {
-    directory: './src/db/migrations'
-  },
-  seeds: {
-    directory: './src/db/seed'
-  }
-}
 
+  // This block is for live Render server
+  production: {
+    client: 'pg',
+    connection: { 
+      connectionString: process.env.DATABASE_URL, 
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: './src/db/migrations'
+    },
+    seeds: {
+      directory: './src/db/seed'
+    }
+  }
 
 };
